34c00623f2302237ecfba477aae67dfddf1336b6
Melanie Sumner <melaniersumner@gmail.com>
2020-11-15 19:40:53 -0600

    added data and defined model

---
+++ app/models/fish.js
@@ -0,0 +1,8 @@
+import Model, { attr } from '@ember-data/model';
+
+export default class FishModel extends Model {
+    @attr name;
+    @attr price;
+    @attr location;
+    @attr season;
+    @attr weather;
